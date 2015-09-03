99dfd5f668af24a314f06367ebe1725ee1512fea
Simone Gentili <sensorario@gmail.com>
2015-09-03 19:35:32 +0000

    Keep php classes in their own file

---
+++ src/Sensorario/Rounding/GoodCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Sensorario\Rounding;
+
+use PHPUnit_Framework_TestCase;
+use Sensorario\Rounding\Good;
+
+class GoodCollection
+{
+    public function isEmpty()
+    {
+        return true;
+    }
+
+    public function items()
+    {
+        return [];
+    }
+}
---
+++ test/unit/Sensorario/Rounding/GoodCollectionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sensorario\Rounding;
+
+use PHPUnit_Framework_TestCase;
+
+final class GoodCollectionTest extends PHPUnit_Framework_TestCase
+{
+    public function testIsEmpty()
+    {
+        $collection = new GoodCollection();
+        $this->assertSame(
+            true,
+            $collection->isEmpty()
+        );
+        $this->assertEquals(
+            [],
+            $collection->items()
+        );
+    }
+}
